2de85a8ffe7849bb967e9d0457deb4a4da7835cc
Aaron Brooks <aaron.brooks@viasat.com>
2018-01-14 19:08:01 -0500

    Add clojure.spec.alpha to project.clj.

--- project.clj
+++ project.clj
@@ -3,4 +3,5 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0-alpha2"]])
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/spec.alpha "0.1.143"]])
